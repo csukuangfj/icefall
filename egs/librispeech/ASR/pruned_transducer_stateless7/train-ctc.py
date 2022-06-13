29d99cd045f83654dbcf79a8c4d3748d3c8fb172
Fangjun Kuang <csukuangfj@gmail.com>
2022-06-13 12:24:36 +0800

    Disable momentum.

--- egs/librispeech/ASR/pruned_transducer_stateless7/train-ctc.py
+++ egs/librispeech/ASR/pruned_transducer_stateless7/train-ctc.py
@@ -935,8 +935,6 @@ def run(rank, world_size, args):
     optimizer = optim.SGD(
         model.parameters(),
         params.initial_lr,
-        momentum=0.9,
-        nesterov=True,
     )
 
     if checkpoints and "optimizer" in checkpoints:
